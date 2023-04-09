473a7db118b1e7e293047bdeeae1d7e74405c801
«IrenSk» <skiv.study@gmail.com>
2023-04-10 00:47:02 +0300

    Hw_1 for send

--- HW_Java/src/hw/hw_1/Main.java
+++ HW_Java/src/hw/hw_1/Main.java
@@ -11,8 +11,8 @@ import hw.hw_1.task3.Hw1_3_0;
 public class Main {
 
     public static void main(String[] args) {
-        // Hw1_1_0.inputNum(); // ЗАДАЧА 1
-        Hw1_2_0.findSimpleNumbers1to1000(); // ЗАДАЧА 2
+        Hw1_1_0.inputNum(); // ЗАДАЧА 1
+        // Hw1_2_0.findSimpleNumbers1to1000(); // ЗАДАЧА 2
         // Hw1_3_0.startCalculator(); // ЗАДАЧА 3
 
     }
