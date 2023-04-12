dd2f398f2b0485174db35e1e08f766c1f2db8ed0
«IrenSk» <skiv.study@gmail.com>
2023-04-13 00:17:11 +0300

    Add solution hw_2 task_1

--- HW_Java/src/hw/hw_2/Main.java
+++ HW_Java/src/hw/hw_2/Main.java
@@ -2,6 +2,6 @@ package hw.hw_2;
 
 public class Main {
     public static void main(String[] args) {
-        // hw.hw_2.Hw1.sortArrNum(); // Задача 1
+        hw.hw_2.Hw1.sortBoobleArrNum(); // Задача 1
     }
 }
