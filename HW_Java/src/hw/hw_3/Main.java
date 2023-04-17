b16988af0ecb937ba703afd6d5f05ea083f7e888
«IrenSk» <skiv.study@gmail.com>
2023-04-17 14:05:42 +0300

    Add solution hw_3 task_2

--- HW_Java/src/hw/hw_3/Main.java
+++ HW_Java/src/hw/hw_3/Main.java
@@ -1,5 +1,8 @@
 package hw.hw_3;
 
+import hw.hw_3.task1.Hw3_1;
+import hw.hw_3.task2.Hw3_2;
+
 /**
  * Main
  **/
@@ -7,8 +10,8 @@ package hw.hw_3;
 public class Main {
 
     public static void main(String[] args) {
-        // Hw1_1_0.inputNum(); // ЗАДАЧА 1
-        
+        // Hw3_1.mergeSortArrList(); // ЗАДАЧА 1
+        Hw3_2.findAndDelEvenNum(); // ЗАДАЧА 2
 
     }
 }
--- HW_Java/src/hw/hw_3/task1/Hw3_1.java
+++ HW_Java/src/hw/hw_3/task1/Hw3_1.java
@@ -1,5 +1,68 @@
+/** Задача 1
+ * Реализовать алгоритм сортировки слиянием
+ * от min к max
+ */
 package hw.hw_3.task1;
 
-public class Hw3_1 {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+public class Hw3_1 {
+    public static ArrayList<Integer> arrayList = new ArrayList<>();
+
+    public static void mergeSortArrList() {
+
+        createRandomIntArrList();
+        System.out.println(arrayList);
+
+        // сделали сортировку на дублекате, но можно было и сразу входящий сортировать
+        ArrayList<Integer> arrayListSortingToMax = new ArrayList<>(arrayList);
+
+        // sortedArrList(arrayListSortingToMax, 0, arrayList.size() - 1);
+
+        System.out.println(arrayListSortingToMax);
+
+    }
+
+    public static void sortedArrList(ArrayList<Integer> arr, int left, int right) {
+        if (right - left < 2)
+            System.out.println(right - left < 2); // для проверки
+        return;
+        int l = left; // left
+        int r = right; // right
+        int center = right / 2;
+
+        while (l <= r) {
+            while (arr.get(l) < center)
+                l++;
+            while (arr.get(r) > center)
+                r--;
+
+            if (l <= r) {
+                int temp = arr.get(l);
+                arr.set(l, arr.get(r));
+                arr.set(r, temp);
+
+            }
+        }
+        if (l < right)
+            sortedArrList(arr, l, right);
+        if (left < r)
+            sortedArrList(arr, left, r);
+        return;
+    }
+
+    public static void createRandomIntArrList() {
+        int size = (int) (Math.random() * 10 + 2); // от 2-х до 11
+        System.out.println("Размер массива: " + size);
+
+        arrayList = new ArrayList<>(size);
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            arrayList.add(random.nextInt(size));
+
+        }
+        // System.out.println(arrayList); // для проверки
+    }
 }
