13bff9b4b4c8dfd711ad870f30f35eb79cf51845
«IrenSk» <skiv.study@gmail.com>
2023-04-22 21:22:17 +0300

    Add solution hw_4 task_1

---
+++ HW_Java/src/hw/hw_4/Hw4_1.java
@@ -0,0 +1,40 @@
+/**Задача 1
+ * Метод, который вернёет «перевёрнутый» список: LinkedList с несколькими элементами
+ */
+package hw.hw_4;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Random;
+import java.util.concurrent.LinkedTransferQueue;
+
+public class Hw4_1 {
+
+    public static final LinkedList<Integer> myLinkedList = new LinkedList<>();
+
+    public static void reverseLinkedList() {
+
+        List<Integer> reverseLL = new LinkedList<>();
+        fillRandomIntLinkedList();
+        // System.arraycopy(myLinkedList, 0, reverseLL, 0, myLinkedList.size());
+        ListIterator<Integer> listIterator = myLinkedList.listIterator(myLinkedList.size());
+        while (listIterator.hasPrevious()) {
+            reverseLL.add(listIterator.previous());
+        }
+
+        System.out.println("В обратном направлении: " + reverseLL);
+
+    }
+
+    public static void fillRandomIntLinkedList() {
+
+        Random random = new Random();
+        int sizeList = 5 + (int) (Math.random() * 10); // от 5 до 14
+        for (int i = 0; i < sizeList; i++) {
+            myLinkedList.add(random.nextInt(-10, 20)); // от -10 до 20
+        }
+
+        System.out.println("Исходный LinkedList:    " + myLinkedList);
+    }
+}
---
+++ HW_Java/src/hw/hw_4/Main.java
@@ -0,0 +1,7 @@
+package hw.hw_4;
+
+public class Main {
+    public static void main(String[] args) {
+        Hw4_1.reverseLinkedList(); // Задача 1
+    }
+}
