134a952e0dd3bbc41d927a2188afe8da5bed9e87
«IrenSk» <skiv.study@gmail.com>
2023-04-24 14:38:26 +0300

    Add solution_0 hw_5 task_1

--- HW_Java/src/hw/hw_5/Hw5_1.java
+++ HW_Java/src/hw/hw_5/Hw5_1.java
@@ -9,6 +9,10 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * ВАЖНО!!! Реализовано с имитацией пользовательских действий (без
+ * пользовательского интерфейса)
+ */
 public class Hw5_1 {
 
     private static final Map<String, ArrayList<String>> NameToPhone_Map = new HashMap<>();
@@ -17,30 +21,124 @@ public class Hw5_1 {
 
     // для имитации заполнения Телефонной книги
     private static ArrayList<String> numsList = new ArrayList<>(
-            Arrays.asList("123456", "456123", "123123", "456789", "789789"));
+            Arrays.asList("123456", "456123", "456789", "789789", "123123", "789000"));
     private static ArrayList<String> namesList = new ArrayList<>(
-            Arrays.asList("Алексеев", "Яковлев", "стилист", "массаж Вера"));
+            Arrays.asList("Алексеев", "Яковлев", "стилист", "массаж"));
 
     public static void createNameToPhoneList() {
 
-        writeNewContact();
+        // Имитация записи новых контактов
+        for (int i = 0; i < namesList.size(); i++) {
+            writeNewContact(i);
+        }
+        showPhoneBook();
 
+        // Имитация изменения текущего контакта
+        String name = "Алексеев";
+        addPhoneInContact(name, 4);
+        showPhoneBook(); // для проверки
+
+        name = "стилист";
+        renameContact(name, "стилист Вера");
+        showPhoneBook(); // для проверки
+
+        // Имитация поиска контакта
+        name = "массаж";
+        showContact(name);
+
+        // Имитация удаления контакта
+        name = "массаж";
+        deleteContact(name);
+        showContact(name); // для проверки
+
+        showPhoneBook();
+    }
+
+    private static void renameContact(String name, String newName) {
+        if (NameToPhone_Map.containsKey(name)) {
+            newPphoneNumsList.addAll(NameToPhone_Map.get(name));
+            NameToPhone_Map.put(newName, new ArrayList<>(newPphoneNumsList));
+
+            System.out.printf("Вы перименовали контакт %s в %s %s %n ", name, newName, NameToPhone_Map.get(newName));
+
+            deleteContact(name);
+        } else {
+            throw new RuntimeException("Такого контакта не найдено.");
+        }
+    }
+
+    private static void deleteContact(String name) {
+        if (NameToPhone_Map.containsKey(name)) {
+
+            NameToPhone_Map.remove(name);
+        } else {
+            throw new RuntimeException("Такого контакта не найдено.");
+        }
+    }
+
+    private static void showContact(String name) {
+        if (NameToPhone_Map.containsKey(name)) {
+            System.out.println("Вы искали: " + name);
+            System.out.printf("%s %s %n", name, NameToPhone_Map.get(name));
+        } else {
+            throw new RuntimeException("Такого контакта не найдено.");
+        }
+    }
+
+    private static void showPhoneBook() {
+        System.out.println("Список контактов: ");
         System.out.println(NameToPhone_Map);
+        System.out.println();
+
     }
 
-    private static void writeNewContact() {
+    private static void addPhoneInContact(String name, int num) {
 
-        newPphoneNumsList.add(numsList.get(0));
-        NameToPhone_Map.put("Алексеев", new ArrayList<>(newPphoneNumsList));
-        newPphoneNumsList.clear();
+        if (NameToPhone_Map.containsKey(name)) {
+            newPphoneNumsList.addAll(NameToPhone_Map.get(name));
+            // newPphoneNumsList.add(numsList.get(2));
+            newPphoneNumsList.add(numsList.get(num));
+            // System.out.println(newPphoneNumsList); // для проверки
+            NameToPhone_Map.put(name, new ArrayList<>(newPphoneNumsList));
 
-        newPphoneNumsList.add("654321");
-        NameToPhone_Map.put("Яковлев", new ArrayList<>(newPphoneNumsList));
-        newPphoneNumsList.clear();
+            System.out.printf("Вы добавили в контакт %s новый телефон: %s %n", name,
+                    NameToPhone_Map.get(name).get(newPphoneNumsList.lastIndexOf(numsList.get(num))));
 
-        newPphoneNumsList = NameToPhone_Map.get("Алексеев");
-        newPphoneNumsList.add("123123");
-        NameToPhone_Map.put("Алексеев", newPphoneNumsList);
-        newPphoneNumsList.clear();
+            newPphoneNumsList.clear();
+            // System.out.println(NameToPhone_Map.get("Алексеев").get(1)); // для проверки
+        } else {
+            throw new RuntimeException("Такого контакта не найдено.");
+        }
     }
+
+    private static void writeNewContact(int i) {
+
+        newPphoneNumsList.add(numsList.get(i));
+        NameToPhone_Map.putIfAbsent(namesList.get(i), new ArrayList<>(newPphoneNumsList));
+        newPphoneNumsList.clear();
+
+    }
+
+    // ЧЕРНОВИК
+    // private static void writeNewContact() {
+    // // String temp = numsList.get(0);
+    // // System.out.println(temp); // для проверки
+    // newPphoneNumsList.add("123456");
+    // // newPphoneNumsList.add(numsList.get(0));
+    // NameToPhone_Map.put("Алексеев", new ArrayList<>(newPphoneNumsList));
+    // newPphoneNumsList.clear();
+
+    // newPphoneNumsList.add("654321");
+    // NameToPhone_Map.put("Яковлев", new ArrayList<>(newPphoneNumsList));
+    // newPphoneNumsList.clear();
+
+    // newPphoneNumsList.addAll(NameToPhone_Map.get("Алексеев"));
+    // // newPphoneNumsList.add(numsList.get(2));
+    // newPphoneNumsList.add("123123");
+    // // System.out.println(newPphoneNumsList); // для проверки
+    // NameToPhone_Map.put("Алексеев", new ArrayList<>(newPphoneNumsList));
+    // newPphoneNumsList.clear();
+
+    // System.out.println(NameToPhone_Map.get("Алексеев").get(1));
+    // }
 }
